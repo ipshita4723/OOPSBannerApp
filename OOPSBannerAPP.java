106d2055f12c9f848f72abb09d8fb5dcfdda66e4
ipshita4723 <ic4723@srmist.edu.in>
2026-02-15 22:05:43 +0530

    Add files via upload

---
+++ OOPSBannerAPP.java
@@ -0,0 +1,23 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to Console
+ * @author Ipshita
+ * @version 2.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+      System.out.println(  " ***** " + " " + " ***** " + " " +" ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ****");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "         **");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "      ** **");
+        System.out.println(" ***** " + " " + " ***** " + " " + " ** " + " " + "   ******* ");
+
+
+    }
+
+
+}
